f30395f351dbf2fedf50afb2cb30ee56231b6528
Allen Huang <allen.huang@allangray.co.za>
2016-08-05 16:06:20 +0200

    WIP: Added auth

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 var config = {}
-// config.concourse_url = "https://YOUR_CONCOURSE_URL_HERE";
-config.concourse_url = "http://localhost:8080";
+config.concourse_url = "https://YOUR_CONCOURSE_URL_HERE";
 config.api_subdirectory = "/api/v1";
+config.concourse_username = "YOUR_CONCOURSE_USERNAME_HERE";
+config.concourse_password = "YOUR_CONCOURSE_PASSWORD_HERE";
+
 module.exports = config;
--- index.js
+++ index.js
@@ -19,11 +19,18 @@ var pipelines;
 get_pipelines = (callback) => {
 	request({
 		url: config.concourse_url + config.api_subdirectory + "/pipelines",
-		json: true
+		auth: {
+			username: config.concourse_username,
+			password: config.concourse_password
+		},
+		json: true,
+		strictSSL: false
 	}, (error, response, body) => {
 		if (!error && response.statusCode === 200) {
 			pipelines = body;
 			callback();
+		} else {
+			console.log(error);
 		}
 	});
 }
@@ -32,13 +39,19 @@ get_pipeline_statuses = () => {
 	for (pipeline of pipelines) {
 		request({
 			url: config.concourse_url + config.api_subdirectory + pipeline.url + "/jobs",
-			json: true
+			auth: {
+				username: config.concourse_username,
+				password: config.concourse_password
+			},
+			json: true,
+			strictSSL: false
 		}, (error, response, body) => {
 			if (!error && response.statusCode === 200) {
 				for (task of body) {
-					if(task.finished_build !== undefined) {
+					if(task.finished_build !== undefined && task.finished_build !== null) {
 						var index = _.findIndex(pipelines, { 'name': task.finished_build.pipeline_name });
-						pipelines[index]["status"] = task.finished_build.status;
+						if(pipelines[index]["status"] === undefined || pipelines[index]["status"] === "succeeded")
+							pipelines[index]["status"] = task.finished_build.status;
 					}
 				}
 			}
@@ -46,13 +59,12 @@ get_pipeline_statuses = () => {
 	}
 }
 
+setInterval(function() {
+	get_pipelines(get_pipeline_statuses);
+}, 5000);
+
 app.get('/', (req, res) => {
 	res.render('overview', { config: config, pipelines: pipelines });
-});
-
-app.get('/refresh_pipelines', (req, res) => {
-	get_pipelines(get_pipeline_statuses);
-	res.status(200).send('complete');
 });
 
 app.listen(app.get('port'), () => {
