5784c53e12d8dbb70c3219a9038f5c55ff35f41b
Allen Huang <allen@allenhuang.net>
2016-08-05 13:12:05 +0200

    WIP

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {}
+// config.concourse_url = "https://YOUR_CONCOURSE_URL_HERE";
+config.concourse_url = "http://localhost:8080";
+config.api_subdirectory = "/api/v1";
+module.exports = config;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var config = require('./config')
+var request = require('request')
+var _ = require('lodash');
 
 var app = express();
 
@@ -10,10 +13,43 @@ app.use(express.static(__dirname + '/resources'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	res.send("Hello world!");
-});
 
+var pipelines;
+var pipeline_statuses = {};
+
+get_pipelines = (callback) => {
+	request({
+		url: config.concourse_url + config.api_subdirectory + "/pipelines",
+		json: true
+	}, (error, response, body) => {
+		if (!error && response.statusCode === 200) {
+			pipelines = body;
+			callback();
+		}
+	});
+}
+
+get_pipeline_statuses = () => {
+	for (pipeline of pipelines) {
+		request({
+			url: config.concourse_url + config.api_subdirectory + pipeline.url + "/jobs",
+			json: true
+		}, (error, response, body) => {
+			if (!error && response.statusCode === 200) {
+				for (task of body) {
+					if(task.finished_build !== undefined) {
+						var index = _.findIndex(pipelines, { 'name': task.finished_build.pipeline_name });
+						pipelines[index]["status"] = task.finished_build.status;
+					}
+				}
+			}
+		});
+	}
+}
+
+app.get('/', (req, res) => {
+	res.render('overview', { config: config, pipelines: pipelines });
+});
 
 app.listen(app.get('port'), () => {
 	console.log('running on port', app.get('port'));
