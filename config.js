789b6e1c82b92a771e4a3226b56510cb79673887
Allen Huang <allen@allenhuang.net>
2016-08-06 13:38:37 +0200

    Updated default concourse url in config to use concourse's demo

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {}
-config.concourse_url = "https://YOUR_CONCOURSE_URL_HERE";
+config.concourse_url = "https://ci.concourse.ci"; //Replace with your concourse url
 config.api_subdirectory = "/api/v1";
 config.concourse_username = "YOUR_CONCOURSE_USERNAME_HERE";
 config.concourse_password = "YOUR_CONCOURSE_PASSWORD_HERE";
