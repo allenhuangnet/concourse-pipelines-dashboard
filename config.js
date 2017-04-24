5702a7f77ac953b39271dd87d877b293aa676bab
innyso <mini.inny@gmail.com>
2017-04-24 14:55:36 +1000

    Externalise configuration
    So that we can pass it in when running the docker container

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 var config = {}
-config.concourse_url = "https://ci.concourse.ci"; //Replace with your concourse url
+config.concourse_url = process.env.CONCOURSE_URL || "https://ci.concourse.ci"; //Replace with your concourse url
 config.api_subdirectory = "/api/v1";
-config.concourse_username = "YOUR_CONCOURSE_USERNAME_HERE";
-config.concourse_password = "YOUR_CONCOURSE_PASSWORD_HERE";
-config.concourse_team="YOUR_CONCOURSE_TEAM_HERE"
-config.use_bearer_token=false
-
+config.concourse_username = process.env.CONCOURSE_USERNAME || "user";
+config.concourse_password = process.env.CONCOURSE_PASSWORD || "password";
+config.concourse_team= process.env.CONCOURSE_TEAM || "main"
+config.use_bearer_token= process.env.CONCOURSE_USE_BEARER || false
 
 module.exports = config;
