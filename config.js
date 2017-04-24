03b04c3e8f41c300e587a10bd31c138004302c61
innyso <mini.inny@gmail.com>
2017-04-24 17:02:08 +1000

    Adding basic auth to dashboard

--- config.js
+++ config.js
@@ -5,5 +5,8 @@ config.concourse_username = process.env.CONCOURSE_USERNAME || "user";
 config.concourse_password = process.env.CONCOURSE_PASSWORD || "password";
 config.concourse_team= process.env.CONCOURSE_TEAM || "main"
 config.use_bearer_token= process.env.CONCOURSE_USE_BEARER || false
+config.enable_basic_auth = process.env.CONCOURSE_ENABLE_BASIC_AUTH || true
+config.basic_auth_user = process.env.CONCOURSE_BASIC_AUTH_USER || "admin"
+config.basic_auth_password = process.env.CONCOURSE_BASIC_AUTH_PASSWORD || "password"
 
 module.exports = config;
